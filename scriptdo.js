4d0c045bbc2d25ef319cc125c033a1f5b60de1be
Aarav02Garg10 <aaravgarg0210@gmail.com>
2024-08-01 22:32:50 +0530

    Add files via upload

---
+++ scriptdo.js
@@ -0,0 +1,8 @@
+//#char at for seeing a string in a arr
+let get=(nam,WTD) => WTD===true?document.getElementsByClassName(nam):document.getElementById(nam);
+let dC=()
+function one(){
+let quetion= get("helo",false);
+const letters="abcdefghijklmnopqrstuvwxyz";
+
+}
